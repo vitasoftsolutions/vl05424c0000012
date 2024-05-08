97ca1accd674011a02bf6b3f7d12f592352968ec
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2024-05-08 10:33:51 +0600

    footer done

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,6 +2,11 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  speed: 1200,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -30,6 +35,11 @@ var swiper = new Swiper(".testimonialSwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1200,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -46,6 +56,64 @@ var swiper = new Swiper(".testimonialSwiper", {
     1280: {
       slidesPerView: 2,
       spaceBetween: 30,
+    },
+  },
+});
+
+var swiper = new Swiper(".universitySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1200,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".btn-uni-next",
+    prevEl: ".btn-uni-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
+
+var swiper = new Swiper(".categorySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1200,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".btn-cat-next",
+    prevEl: ".btn-cat-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 6,
+      spaceBetween: 20,
     },
   },
 });
